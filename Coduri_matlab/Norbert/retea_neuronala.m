346ad22eb12fc72eb7a357003cb72b462ccab262
Horvat Norbert <horvatnor24o@gmail.com>
2020-08-17 13:04:28 +0300

    cod

--- Coduri_matlab/Norbert/retea_neuronala.m
+++ Coduri_matlab/Norbert/retea_neuronala.m
@@ -1,38 +1,43 @@
-%% date simulate 
+%% incarcare date
 
-%%incarcare date
+inputs=data_zile'
+targets=cazuri_totale_ro_geo'
 
-%Basuc fitting data cazuri_totale_ro_geo
-%Coefficients:
-  p1 = 0.00046611;
-  p2 = -0.15162;
-  p3 = 16.913;
-  p4 = -493.66;
-  p5 = 2918.5;
-  x=0:1:178;
-y = p1*x.^4 + p2*x.^3 + p3*x.^2 + p4*x + p5 ;
-figure
-plot(x,y)
-hold on
-plot(t_ro_geo,cazuri_totale_ro_geo')
+net = feedforwardnet(20,'trainlm');
+net.divideParam.trainRatio = 70/100;
+net.divideParam.valRatio = 15/100;
+net.divideParam.testRatio = 15/100;
+net.trainParam.goal=0.0001;
+net.trainparam.epochs = 5000;     
+net.trainParam.max_fail = 500;
+%% training
+     net2 = train(net,inputs,targets);
 
-%Norm of residuals = 9256.4
+%% plot
+u=0:1: 300
+y=sim(net2,u)' ;
 
+plot(u,y,'r')
+hold on 
+plot(data_zile,targets','b')
+     
+	 
+%% incarcare date
+inputs=data_zile'
+targets=cazuri_totale_ro_geo'
 
-%% cu retele neuronale 
-x =  y;
-t = cazuri_totale_ro_geo';
+net = newff(inputs,targets,20);
 
-net = feedforwardnet(10);
-net = train(net,x,t);
-view(net)
-y = net(x);
-perf = perform(net,y,t)
+ net.trainParam.goal=0.0001;
+net.trainparam.epochs = 5000;     
+net.trainParam.max_fail = 500;
+%% training
+  net2 = train(net,inputs,targets);
+%% plot
+u=0:1:300
+y=sim(net2,u)' ;
 
-
-%%
-plot(t_ro_geo,net(cazuri_totale_ro_geo'))
-hold on
-plot(t_ro_geo,cazuri_totale_ro_geo')
-legend('neural ','initial ')
-
+plot(u,y,'r')
+hold on 
+plot(data_zile,targets','b')
+     
